3e4402226e4f27f467ea4b356ddeb1d5568588cf
bobberr <izotopenko@gmail.com>
2018-06-22 16:40:51 +0300

    FIrst commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        wefewf
+      </div>
+    );
+  }
+}
+
+export default App;
